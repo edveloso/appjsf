5a432c05ef00ca46973bd47a16716a59a5e44c00
ednilson.moura <ednilson.moura@SJ2161S-099.lab.infnet.com.br>
2020-03-07 14:04:29 -0300

    Managedbean

--- src/main/java/web/bean/Produto.java
+++ src/main/java/web/bean/Produto.java
@@ -2,4 +2,35 @@ package web.bean;
 
 public class Produto {
 
+	private String nome;
+	
+	private Double preco;
+
+	public Produto(String nome, double preco) {
+		this.nome = nome;
+		this.preco = preco;
+	}
+	
+	public Produto() {
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Double getPreco() {
+		return preco;
+	}
+
+	public void setPreco(Double preco) {
+		this.preco = preco;
+	}
+	
+	
+	
+	
 }
--- src/main/java/web/bean/ProdutoBean.java
+++ src/main/java/web/bean/ProdutoBean.java
@@ -1,17 +1,47 @@
 package web.bean;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
+import javax.faces.model.DataModel;
+import javax.faces.model.ListDataModel;
 
 @ManagedBean
 @RequestScoped
 public class ProdutoBean {
 
 	private Produto produto;
+	private static List<Produto> lista;
 
+	private DataModel<Produto> produtos;
+	
 	public String adicionaForm() {
 		produto = new Produto();		
 		return "produto_form";
 	}
+
+	public Produto getProduto() {
+		return produto;
+	}
+
+	public void setProduto(Produto produto) {
+		this.produto = produto;
+	}
+
+	public DataModel<Produto> getProdutos() {
+		lista = new ArrayList<Produto>();
+		lista.add(new Produto("Iphone", 9000.0));
+		lista.add(new Produto("Xiaomi", 200.0));
+		produtos = new ListDataModel<Produto>(lista);
+		return produtos;
+	}
+
+	public void setProdutos(DataModel<Produto> produtos) {
+		this.produtos = produtos;
+	}
+
+	
 	
 }
